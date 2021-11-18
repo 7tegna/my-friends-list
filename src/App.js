bfdd8fbc5e3a62b28c0de7e56fa09b1f6543f874
Tsegaye Alemayehu <tsegayealemayehu@imac.myfiosgateway.com>
2021-11-18 17:31:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {React, useState } from 'react';
+import Main from './components/Main'
+import Store from './Store';
+
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[store, setStore] = useState(Store);
+  
+    return(
+      <div className="App">
+        <h1> My Friends Profile. </h1>
+        <Main main={store}/> 
+  
+      </div>
+  ) 
 }
 
 export default App;
